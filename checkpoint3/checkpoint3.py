34ce2292ec41495fe894ba28b1111ab138e93160
rinones <rinonesdiez@gmail.com>
2025-06-03 21:53:37 +0200

    Checkpoint 2 terminado e inicio del checkpoint 3
    Se han añadido los documentos del checkpoint 2 a la propia tarjeta y se hará así con los siguientes checkpoints.
    
    Se inicia el checkpoint 3.

---
+++ checkpoint3/checkpoint3.py
@@ -0,0 +1,10 @@
+nombre = 'Checkpoint'
+leccion = 3
+lista = [1, 2, 3, 4, 5]
+estudiado = True
+
+
+# Además necesito que me crees una cadena que contenga la palabra "Hola". Usando la palabra 
+# clave en el método de búsqueda o el índice, busque y seleccione "Hola" en su cadena. Y usando 
+# la función de reemplazo, reemplace "Hola" en su cadena con "adiós".
+# Este ejercicio de Python debes subirlo a tu Git-Hub o Replit para poder revisarlo
