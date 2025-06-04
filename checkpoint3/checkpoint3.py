dfc392ec8323a77f5edadcbf3a1e2b64efa64174
rinones <rinonesdiez@gmail.com>
2025-06-04 13:55:21 +0200

    Checkpoint 3 terminado
    Entrega final del checkpoint 3. El .pdf con las respuestas teóricas se encuentra en el repositorio, en la carpeta docs.

--- checkpoint3/checkpoint3.py
+++ checkpoint3/checkpoint3.py
@@ -1,10 +1,89 @@
-nombre = 'Checkpoint'
+# Ejercicio 1
+print('------')
+nombre = 'Check Point'
 leccion = 3
 lista = [1, 2, 3, 4, 5]
 estudiado = True
 
+# Ejercicio 2
+print('------')
+recorte = nombre[:3]
 
-# Además necesito que me crees una cadena que contenga la palabra "Hola". Usando la palabra 
-# clave en el método de búsqueda o el índice, busque y seleccione "Hola" en su cadena. Y usando 
-# la función de reemplazo, reemplace "Hola" en su cadena con "adiós".
-# Este ejercicio de Python debes subirlo a tu Git-Hub o Replit para poder revisarlo
+print(recorte)
+
+# Ejercicio 3
+print('------')
+elemento = lista[0]
+
+print(elemento)
+
+# Ejercicio 4
+print('------')
+
+plus_ten = leccion + 10
+
+print(plus_ten)
+
+# Ejercicio 5
+print('------')
+
+last_element = nombre[-5:]
+
+print(last_element)
+
+# Ejercicio 6
+print('------')
+
+names = 'harry,alex,susie,jared,gail,conner'
+
+names_list = names.split()
+
+print(names_list)
+
+# Ejercicio 7
+
+print('------')
+first_word = nombre[0:5]
+
+print(first_word)
+
+print('------')
+first_word_upper = first_word.upper()
+
+print(first_word_upper)
+
+sentence = first_word_upper + nombre[5:]
+
+print(sentence)
+
+# Ejercicio 8
+print('------')
+
+sentence_int = f"This is the {nombre} number {leccion}."
+
+print(sentence_int)
+
+# Ejercicion 9
+print('------')
+
+print('Hello world')
+
+# Ejercicio extra
+print('------')
+
+frase = '"Hola" is how to say "Hello" in English'
+
+print(frase)
+
+word_index = frase.find('Hola')
+
+print(word_index)
+
+palabra_cortada = frase[word_index:word_index + 4]
+
+print(palabra_cortada)
+
+frase_nueva = frase.replace('Hola', 'adios')
+
+print(frase_nueva)
+
